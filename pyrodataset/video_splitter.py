1c74d16577f83512f2ecf7977a9e72f522b00c72
Mateo LOSTANLEN <mateo.lostanlen@gmail.com>
2022-02-14 17:08:24 +0100

    add header

--- pyrodataset/video_splitter.py
+++ pyrodataset/video_splitter.py
@@ -1,3 +1,8 @@
+# Copyright (C) 2019-2022, Pyronear.
+
+# This program is licensed under the Apache License version 2.
+# See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
+
 import os
 import pickle
 from bisect import bisect_left, bisect_right
