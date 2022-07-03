4c044615b49a982efc210aee92b528fc2c17e2c8
Mateo LOSTANLEN <mateo.lostanlen@gmail.com>
2022-07-03 17:33:43 +0200

    auto label stage

---
+++ pipeline/auto_label.py
@@ -0,0 +1,23 @@
+import subprocess
+from ruamel.yaml import YAML
+import glob
+import os
+
+
+# Load Params
+with open("params.yaml", "r") as fd:
+    yaml = YAML()
+    params = yaml.load(fd)
+
+models = params["auto_label"]["models"]
+img_size = params["auto_label"]["img_size"]
+conf = params["auto_label"]["conf"]
+
+folders = glob.glob('Data/All_Data/**/**/images')
+
+for folder in folders:
+    if not os.path.isdir(folder.replace('images', 'labels')):
+        name = folder.split('All_Data')[1].split('images')[0][1:]
+        cmd = f"python yolov5/detect.py --weights {models} --img {img_size} --conf {conf} --source {folder}  --nosave \
+               --save-txt --save-conf --project Data/Labels --name {name}"
+        subprocess.call(cmd, shell=True)
