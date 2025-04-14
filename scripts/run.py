df58c884f78d17faf08152d973be31c5ade17352
Arthur Caillau <arthur@caillau.me>
2025-04-14 12:29:39 +0200

    chore: cleanup unused files

--- scripts/run.py
+++
@@ -1,4 +0,0 @@
-import pyro_dataset.utils as utils
-
-if __name__ == "__main__":
-    utils.greet("chouffe")
--- src/pyro_dataset/utils.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-def greet(name: str) -> None:
-    print(f"Hello {name}")
-
-
-def parse_label_filepath(filepath_label: Path) -> dict:
-    return {}
