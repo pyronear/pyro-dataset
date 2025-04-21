a44ff572cbb7137a16fc9ee2cb7ea1949d7431f2
Arthur Caillau <arthur@caillau.me>
2025-04-21 15:21:28 +0200

    feat(dvc): add stage split_false_positives

---
+++ scripts/split_false_positives.py
@@ -0,0 +1,209 @@
+"""
+CLI Script to perform a data split from the FP_2024 dataset to assign images into a train, val, test split.
+
+The folder structure will follow a ultralytics YOLO scaffolding.
+"""
+
+import argparse
+import logging
+import random
+import shutil
+from dataclasses import dataclass
+from pathlib import Path
+
+from tqdm import tqdm
+
+
+@dataclass
+class DataSplit:
+    """
+    Simple class to store the result of the datasplit.
+
+    Attributes:
+        train: contains a list of image filepaths and makes the train split.
+        val: contains a list of image filepaths and makes the val split.
+        test: contains a list of image filepaths and makes the test split.
+    """
+
+    train: list[Path]
+    val: list[Path]
+    test: list[Path]
+
+
+def make_cli_parser() -> argparse.ArgumentParser:
+    """
+    Make the CLI parser.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--save-dir",
+        help="directory to save the splitted dataset.",
+        type=Path,
+        default=Path("./data/interim/data-split/false_positives/FP_2024/wise_wolf/"),
+    )
+    parser.add_argument(
+        "--dir-dataset",
+        help="directory containing the false positives dataset.",
+        type=Path,
+        default=Path("./data/interim/filtered/false_positives/FP_2024/wise_wolf/"),
+    )
+    parser.add_argument(
+        "--random-seed",
+        help="Random Seed to perform the data split",
+        type=int,
+        required=True,
+        default=0,
+    )
+    parser.add_argument(
+        "--ratio-train-val",
+        help="Ratio for splitting train and val splits",
+        type=float,
+        default=0.9,
+    )
+    parser.add_argument(
+        "--ratio-val-test",
+        help="Ratio for splitting val and test splits",
+        type=float,
+        default=0.5,
+    )
+    parser.add_argument(
+        "-log",
+        "--loglevel",
+        default="info",
+        help="Provide logging level. Example --loglevel debug, default=warning",
+    )
+    return parser
+
+
+def validate_parsed_args(args: dict) -> bool:
+    """
+    Return whether the parsed args are valid.
+    """
+    if not args["dir_dataset"].exists():
+        logging.error(
+            f"invalid --dir-dataset, dir {args['dir_dataset']} does not exist"
+        )
+        return False
+    else:
+        return True
+
+
+def make_data_split(
+    dir_dataset: Path,
+    random_seed: float,
+    ratio_train_val: float,
+    ratio_val_test: float,
+) -> DataSplit:
+    """
+    Perform the data split for the FP_2024 dataset using the
+    provided `random_seed` and the `ratio_train_val`
+
+    __Note__: Prevent train/val leakage by splitting at the folder level.
+
+    Returns:
+        data_split (DataSplit): the data split.
+
+    Throws:
+        assertError: when the ratios are not in the right ranges (0..1)
+    """
+
+    assert 0 <= ratio_train_val <= 1.0, "ratio_train_val should be in (0..1)"
+    assert 0 <= ratio_val_test <= 1.0, "ratio_val_test should be in (0..1)"
+
+    rng = random.Random(random_seed)
+    folders = list_directories(dir_dataset)
+    folders_shuffled = rng.sample(folders, len(folders))
+    number_folders = len(folders)
+
+    filepaths_images_train = []
+    filepaths_images_val = []
+    filepaths_images_test = []
+
+    for idx, folder in enumerate(folders_shuffled):
+        filepaths_images = list(folder.glob("**/*.jpg"))
+        if idx < number_folders * ratio_train_val:
+            filepaths_images_train.extend(filepaths_images)
+        elif idx < number_folders * (
+            ratio_train_val + (1 - ratio_train_val) * ratio_val_test
+        ):
+            filepaths_images_val.extend(filepaths_images)
+        else:
+            filepaths_images_test.extend(filepaths_images)
+    return DataSplit(
+        train=filepaths_images_train,
+        val=filepaths_images_val,
+        test=filepaths_images_test,
+    )
+
+
+def list_directories(dir_dataset: Path) -> list[Path]:
+    """
+    List all directories in the `dir_dataset`.
+
+    Returns:
+        dirs (list[Path]): list of directories.
+    """
+    return [d for d in dir_dataset.iterdir() if d.is_dir()]
+
+
+def persist_data_split(
+    data_split: DataSplit,
+    save_dir: Path,
+) -> None:
+    """
+    Persist the data_split in save_dir, structure it in a regular yolo folder structure.
+
+    Returns:
+        None
+    """
+    for split, split_xs in [
+        ("train", data_split.train),
+        ("val", data_split.val),
+        ("test", data_split.test),
+    ]:
+        for filepath_image in tqdm(split_xs):
+            filepath_image_destination = (
+                save_dir / "images" / split / filepath_image.name.lower()
+            )
+            filepath_label_destination = (
+                save_dir / "labels" / split / f"{filepath_image.stem}.txt".lower()
+            )
+            filepath_image_destination.parent.mkdir(parents=True, exist_ok=True)
+            filepath_label_destination.parent.mkdir(parents=True, exist_ok=True)
+            shutil.copy(filepath_image, filepath_image_destination)
+            filepath_label_destination.touch()
+
+
+if __name__ == "__main__":
+    cli_parser = make_cli_parser()
+    args = vars(cli_parser.parse_args())
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(level=args["loglevel"].upper())
+    if not validate_parsed_args(args):
+        exit(1)
+    else:
+        logging.info(args)
+        save_dir = args["save_dir"]
+        dir_dataset = args["dir_dataset"]
+        random_seed = args["random_seed"]
+        ratio_train_val = args["ratio_train_val"]
+        ratio_val_test = args["ratio_val_test"]
+
+        logger.info(f"save results in {save_dir}")
+        save_dir.mkdir(parents=True, exist_ok=True)
+        folders = list_directories(dir_dataset)
+        logger.info(f"found {len(folders)} directories in {dir_dataset}")
+
+        logger.info("split the data in train, val, test")
+        data_split = make_data_split(
+            dir_dataset=dir_dataset,
+            random_seed=random_seed,
+            ratio_train_val=ratio_train_val,
+            ratio_val_test=ratio_val_test,
+        )
+        logger.info(
+            f"datasplit: {len(data_split.train)} images in train - {len(data_split.val)} images in val - {len(data_split.test)} images in test."
+        )
+        logger.info(f"persist the data split in {save_dir}.")
+        persist_data_split(data_split=data_split, save_dir=save_dir)
+        exit(0)
