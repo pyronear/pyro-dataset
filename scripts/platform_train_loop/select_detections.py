70c21438af166d155882e405fead001c34bae605
Arthur Caillau <arthur@caillau.me>
2025-06-18 11:33:33 +0200

    feat: adapt the select_detections.py script to copy over the gt

--- scripts/platform_train_loop/select_detections.py
+++ scripts/platform_train_loop/select_detections.py
@@ -162,7 +162,13 @@ def get_filepaths(dir_sequence: Path, stem: str) -> dict[str, Path]:
         dict[str, Path]: A dictionary containing the file paths for labels, images, and detections.
     """
     return {
-        "filepath_label": dir_sequence / "labels" / f"{stem}.txt",
+        # "filepath_label": dir_sequence / "labels" / f"{stem}.txt",
+        "filepath_label_ground_truth": dir_sequence
+        / "labels_ground_truth"
+        / f"{stem}.txt",
+        "filepath_label_prediction": dir_sequence
+        / "labels_predictions"
+        / f"{stem}.txt",
         "filepath_image": dir_sequence / "images" / f"{stem}.jpg",
         "filepath_detection": dir_sequence / "detections" / f"{stem}.jpg",
     }
@@ -282,19 +288,40 @@ def copy_over(
             / "detections"
             / filepath_source_detection.name
         )
-        filepath_source_label = record["filepath_label"]
-        filepath_destination_label = (
+        filepath_source_label_prediction = record["filepath_label_prediction"]
+        filepath_destination_label_prediction = (
             dir_save
             / dir_sequence.relative_to(dir_platform_annotated_sequences)
-            / "labels"
-            / filepath_source_label.name
+            / "labels_predictions"
+            / filepath_source_label_prediction.name
+        )
+        filepath_source_label_ground_truth = record["filepath_label_ground_truth"]
+        filepath_destination_label_ground_truth = (
+            dir_save
+            / dir_sequence.relative_to(dir_platform_annotated_sequences)
+            / "labels_ground_truth"
+            / filepath_source_label_ground_truth.name
         )
         filepath_destination_image.parent.mkdir(parents=True, exist_ok=True)
         filepath_destination_detection.parent.mkdir(parents=True, exist_ok=True)
-        filepath_destination_label.parent.mkdir(parents=True, exist_ok=True)
+        # filepath_destination_label.parent.mkdir(parents=True, exist_ok=True)
+        filepath_destination_label_prediction.parent.mkdir(parents=True, exist_ok=True)
+        filepath_destination_label_ground_truth.parent.mkdir(
+            parents=True, exist_ok=True
+        )
         shutil.copy(src=filepath_source_image, dst=filepath_destination_image)
         shutil.copy(src=filepath_source_detection, dst=filepath_destination_detection)
-        shutil.copy(src=filepath_source_label, dst=filepath_destination_label)
+        # shutil.copy(src=filepath_source_label, dst=filepath_destination_label)
+        shutil.copy(
+            src=filepath_source_label_prediction,
+            dst=filepath_destination_label_prediction,
+        )
+
+        if filepath_destination_label_ground_truth.exists():
+            shutil.copy(
+                src=filepath_source_label_ground_truth,
+                dst=filepath_destination_label_ground_truth,
+            )
 
 
 if __name__ == "__main__":
